0ec2564117caa82ecf7cbd5dfcbb0d47a26652a0
Martin Herstad <mherstad@spotify.com>
2019-05-04 15:11:04 +0200

    Refactored Orders to use function Components

--- src/Orders.js
+++ src/Orders.js
@@ -1,53 +1,58 @@
-import React, {Component} from 'react';
+import React, {useContext} from 'react';
 import BrigandContext from "./BrigandContext";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import {getItemById, getItemsByPlayer} from "./itemsUtil";
 
-class Orders extends Component {
+const selectedHasAp = (selectedId, items) => {
+  const ap = getItemById(selectedId, items).ap;
+  return ap > 0;
+};
 
-  selectedHasAp = (selectedId, items) => {
-    const ap = getItemById(selectedId, items).ap;
-    return ap > 0;
-  };
-
-  render() {
-    return <div>
-      <Card>
-        <CardContent>
-          <BrigandContext.Consumer>
-            {({endTurn, turn}) => {
-              return (
-                <Button onClick={endTurn}>Turn: {turn}</Button>
-              )
-            }}
-          </BrigandContext.Consumer>
-          <BrigandContext.Consumer>
-            {({moveTowardEnemy, selectedId, items}) => {
-              return (this.selectedHasAp(selectedId, items) ?
-                <Button onClick={moveTowardEnemy}>Move Toward Enemy</Button> : undefined)
-            }}
-          </BrigandContext.Consumer>
-          <BrigandContext.Consumer>
-            {({moveAwayFromEnemy, selectedId, items}) => {
-              return (this.selectedHasAp(selectedId, items) ?
-                <Button onClick={moveAwayFromEnemy}>Move Away From Enemy</Button> : undefined)
-            }}
-          </BrigandContext.Consumer>
-          <BrigandContext.Consumer>
-            {({attack, inRange, selectedId, items}) => {
-              const selected = getItemById(selectedId, items);
-              //TODO create a button for each attack option
-              const target = getItemsByPlayer('ai', items).find((enemy) => inRange(selected, enemy));
-              return (this.selectedHasAp(selectedId, items) && target ?
-                <Button onClick={attack}>Attack!</Button> : undefined)
-            }}
-          </BrigandContext.Consumer>
-        </CardContent>
-      </Card>
-    </div>
-  }
+function TurnButton() {
+  const {endTurn, turn} = useContext(BrigandContext);
+  return (
+    <Button onClick={endTurn}>Turn: {turn}</Button>
+  );
 }
 
-export default Orders;
+function MoveTowardEnemyButton() {
+  const {moveTowardEnemy, selectedId, items} = useContext(BrigandContext);
+  if (!selectedHasAp(selectedId, items)) {
+    return null;
+  }
+  return (<Button onClick={moveTowardEnemy}>Move Toward Enemy</Button>);
+}
+
+function MoveAwayFromEnemyButton() {
+  const {moveAwayFromEnemy, selectedId, items} = useContext(BrigandContext);
+  if (!selectedHasAp(selectedId, items)) {
+    return null;
+  }
+  return (<Button onClick={moveAwayFromEnemy}>Move Away From Enemy</Button>);
+}
+
+function AttackButton() {
+  const {attack, inRange, selectedId, items} = useContext(BrigandContext);
+  const selected = getItemById(selectedId, items);
+  //TODO create a button for each attack option
+  const target = getItemsByPlayer('ai', items).find((enemy) => inRange(selected, enemy));
+  if (!selectedHasAp(selectedId, items) || !target) {
+    return null;
+  }
+  return (<Button onClick={attack}>Attack!</Button>);
+}
+
+export default function Orders() {
+  return <div>
+    <Card>
+      <CardContent>
+        <TurnButton/>
+        <MoveTowardEnemyButton/>
+        <MoveAwayFromEnemyButton/>
+        <AttackButton/>
+      </CardContent>
+    </Card>
+  </div>
+}
