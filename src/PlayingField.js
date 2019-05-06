ca0c51152f136d65a0fdc63e3ee10e0b017de454
Martin Herstad <mherstad@spotify.com>
2019-05-06 12:45:00 +0200

    Refactored Component as function

--- src/PlayingField.js
+++ src/PlayingField.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useContext} from 'react';
 import BrigandContext from "./BrigandContext";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,56 +7,58 @@ import TableRow from '@material-ui/core/TableRow';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 
-class PlayingField extends Component {
 
-  typeIcons = {
-    o: 'android',
-    x: 'directions_walk',
-    mounted: 'direction_bike',
-    grass: 'crop_free',
-    tree:'nature',
-    water: 'waves',
-    rock: 'landscape',
-    dead: 'airline_seat_flat',
+const typeIcons = {
+  o: 'android',
+  x: 'directions_walk',
+  mounted: 'direction_bike',
+  grass: 'crop_free',
+  tree: 'nature',
+  water: 'waves',
+  rock: 'landscape',
+  dead: 'airline_seat_flat',
 
-  };
+};
 
-  getIcon = (type) => this.typeIcons[type] || 'crop_free';
+const getIcon = (type) => typeIcons[type] || 'crop_free';
 
-  render() {
-
-    return <div className="PlayingField">
-      <Table padding="none">
-        <TableBody>
-        <BrigandContext.Consumer>
-          {({items, setSelected}) => {
-            return (this.createMatrix(10, items).map(row =>
-              <TableRow key={row[0].y}>{row.map(elem =>
-                <TableCell key={'x' + elem.x + 'y' + elem.y}>
-                  <IconButton onClick={() => setSelected(elem.id)}><Icon>{this.getIcon(elem.type)}</Icon></IconButton>
-                </TableCell>)}
-              </TableRow>))
-          }}
-        </BrigandContext.Consumer>
-        </TableBody>
-      </Table>
-    </div>;
-  }
-
-  createMatrix(n = 10, items) {
-    let matrix = [];
-    for (let y = 0; y < n; y++) {
-      matrix[y] = [];
-      for (let x = 0; x < n; x++) {
-        matrix[y][x] = this.createAtPosition(items, x, y);
-      }
+const createMatrix = (n = 10, items) => {
+  let matrix = [];
+  for (let y = 0; y < n; y++) {
+    matrix[y] = [];
+    for (let x = 0; x < n; x++) {
+      matrix[y][x] = createAtPosition(items, x, y);
     }
-    return matrix;
   }
+  return matrix;
+};
 
-  createAtPosition(items = [], x, y) {
-    return items.find((item) => item.x === x && item.y === y) || {x: x, y: y, type: '.'}
-  }
+const createAtPosition = (items = [], x, y) => {
+  return items.find((item) => item.x === x && item.y === y) || {x: x, y: y, type: '.'}
+};
+
+function PlayingFieldCell({elem}) {
+  const {setSelected} = useContext(BrigandContext);
+  return (
+    <TableCell key={'x' + elem.x + 'y' + elem.y}>
+      <IconButton
+        onClick={() => setSelected(elem.id)}><Icon>{getIcon(elem.type)}</Icon></IconButton>
+    </TableCell>);
 }
 
-export default PlayingField;
+export default function PlayingField() {
+  const {items} = useContext(BrigandContext);
+  const matrix = createMatrix(10, items);
+  return <div className="PlayingField">
+    <Table padding="none">
+      <TableBody>
+        {
+          (matrix.map(row =>
+            <TableRow key={row[0].y}>{
+              row.map(elem => <PlayingFieldCell elem={elem}/>)}
+            </TableRow>))
+        }
+      </TableBody>
+    </Table>
+  </div>;
+}
