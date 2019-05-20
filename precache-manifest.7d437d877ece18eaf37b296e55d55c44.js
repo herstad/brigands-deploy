96ab5d75e3058caa8745d77fb0756838cc979210
Martin Herstad <mherstad@spotify.com>
2019-05-20 08:35:36 +0200

    Updates

---
+++ precache-manifest.7d437d877ece18eaf37b296e55d55c44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "248a565ce1fb37be803a",
+    "url": "/static/css/main.c4688234.chunk.css"
+  },
+  {
+    "revision": "248a565ce1fb37be803a",
+    "url": "/static/js/main.b4973be4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d93d2e5d7210e4fc73b9",
+    "url": "/static/js/2.d1df85a7.chunk.js"
+  },
+  {
+    "revision": "5011d582af52ee66705b9dd3f5317624",
+    "url": "/index.html"
+  }
+];
